3daa142a4686a6989a503339ba89457bd77197c9
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-06-07 03:22:06 -0300

    Desafios 10, 11 e 12

--- Desafios.sql
+++ Desafios.sql
@@ -54,3 +54,32 @@ SELECT
 FROM Atores
 WHERE Genero = 'F'
 ORDER BY PrimeiroNome
+
+
+-- 10
+SELECT
+	f.Nome,
+	g.Genero
+FROM Filmes f
+INNER JOIN FilmesGenero fg ON f.Id = fg.IdFilme
+INNER JOIN Generos g ON g.Id = fg.IdGenero
+
+
+-- 11
+SELECT
+	f.Nome,
+	g.Genero
+FROM Filmes f
+INNER JOIN FilmesGenero fg ON f.Id = fg.IdFilme
+INNER JOIN Generos g ON g.Id = fg.IdGenero
+WHERE Genero = 'Mistério'
+
+-- 12
+SELECT
+	f.Nome,
+	a.PrimeiroNome,
+	a.UltimoNome,
+	ef.Papel
+FROM Filmes f
+INNER JOIN ElencoFilme ef ON f.Id = ef.IdFilme
+INNER JOIN Atores a ON a.Id = ef.IdAtor
